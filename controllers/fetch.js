2a4875d9410d1db2f3606b8998da61d15aaa6213
Nala <sandy.enow1@gmail.com>
2019-11-16 15:43:00 -0500

    worked on server.js

---
+++ controllers/fetch.js
@@ -0,0 +1,18 @@
+// Controller for our scraper
+// ============================
+var db = require("../models");
+var scrape = require("../scripts/scrape");
+
+module.exports = {
+  scrapeHeadlines: function(req, res) {
+    // scrape the NYT
+    return scrape()
+      .then(function(articles) {
+        
+        console.log('articles',articles);
+        // then insert articles into the db
+        
+      })
+      
+  }
+};
